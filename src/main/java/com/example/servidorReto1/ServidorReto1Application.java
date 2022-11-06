12c5936f40cd6c0bd19a925bd39681961166f640
DESKTOP-O72PH1Q\Esti <estibalizmares@gmail.com>
2022-11-06 17:51:37 +0100

    Initial commit

---
+++ src/main/java/com/example/servidorReto1/ServidorReto1Application.java
@@ -0,0 +1,13 @@
+package com.example.servidorReto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorReto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorReto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servidorReto1/ServidorReto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidorReto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorReto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
